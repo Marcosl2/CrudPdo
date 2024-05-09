ab5b4ccbdfefc5b28db06ba71c1c1d58259d329f
Marcosl2 <lamed-4@hotmail.com>
2024-05-09 15:23:34 -0300

    Adicionando validações no campo email

--- bd_cadastrar.php
+++ bd_cadastrar.php
@@ -3,13 +3,27 @@
 include 'config.php';
 
 
-$nome = filter_input(INPUT_POST,'nome');
-$email = filter_input(INPUT_POST,'email');
+$nome = filter_input(INPUT_POST, 'nome');
+$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
 
-$sql = $pdo->prepare("INSERT INTO usuario (nome,email) VALUES (:nome,:email)");
+if ($nome && $email) {
 
-$sql->bindValue(':nome',$nome);
-$sql->bindValue(':email', $email);
-$sql->execute();
+    $sql = $pdo->prepare("SELECT * FROM usuario WHERE email = :email");
+    $sql->bindValue(':email', $email);
+    $sql->execute();
+    if ($sql->rowCount() === 0) {
+        $sql = $pdo->prepare("INSERT INTO usuario (nome,email) VALUES (:nome,:email)");
 
-header('Location:index.php');
+        $sql->bindValue(':nome', $nome);
+        $sql->bindValue(':email', $email);
+        $sql->execute();
+
+        header('Location:index.php');
+        exit;
+    }else{
+        header('Location:cadastrar.php');
+    }
+} else {
+    header('Location:cadastrar.php');
+    exit;
+}
