c3a53eb399ef511a7f0efbaa91fb46da356222d5
Marcosl2 <lamed-4@hotmail.com>
2024-05-10 10:36:26 -0300

    Criado a Lista de Usuários

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <?php
 include 'config.php';
+$results = [];
+
+$sql = $pdo->query("SELECT * FROM usuario");
+if ($sql->rowCount() > 0) {
+    $results = $sql->fetchAll(PDO::FETCH_ASSOC);
+}
 
 ?>
 
-<h1>Listagem de Usuarios</h1>
+<h1 style="color: blue;">Lista de Usuários</h1>
 <div style="margin-bottom: 5px;">
     <table border="1">
         <thead>
@@ -15,16 +21,23 @@ include 'config.php';
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td>1</td>
-                <td>Marcelo</td>
-                <td>Maria</td>
-            </tr>
+            <?php foreach ($results as $result) : ?>
+                <tr>
+                    <td><?= $result['idusuario']; ?></td>
+                    <td><?= $result['nome']; ?></td>
+                    <td><?= $result['email']; ?></td>
+                    <td>
+                        <a href="editar.php?id=<?= $result['idusuario'] ?>"> [Editar] </a> 
+
+                        <a href="excluir.php?id=<?= $result['idusuario'] ?>"> [Excluir] </a>
+                    <td>
+                </tr>
+            <?php endforeach; ?>
         </tbody>
     </table>
 </div>
 
 
 <div>
-    <a href="cadastrar.php"><button> Cadastrar Usuario</button></a>
+    <a href="cadastrar.php"><button> Cadastro de Usuario</button></a>
 </div>
